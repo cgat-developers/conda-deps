153373c8dabedebb620d0579bda589879945b1c0
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2019-03-16 20:19:29 +0100

    improved test script

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,74 @@
 #!/usr/bin/env bash
 
-diff <(python3 conda-deps.py tests/experiment.py) <(cat tests/experiment.env)
-diff <(python3 conda-deps.py tests/IndexedFasta.py) <(cat tests/IndexedFasta.env)
-diff <(python3 conda-deps.py tests/pipeline_rnaseqqc.py) <(cat tests/pipeline_rnaseqqc.env)
+# References
+# http://kvz.io/blog/2013/11/21/bash-best-practices/
+# http://jvns.ca/blog/2017/03/26/bash-quirks/
+
+# exit when a command fails
+set -o errexit
+
+# exit if any pipe commands fail
+set -o pipefail
+
+# exit when your script tries to use undeclared variables
+#set -o nounset
+
+# trace what gets executed
+set -o xtrace
+
+# Bash traps
+# http://aplawrence.com/Basics/trapping_errors.html
+# https://stelfox.net/blog/2013/11/fail-fast-in-bash-scripts/
+
+SCRIPT_NAME="$0"
+SCRIPT_PARAMS="$@"
+
+error_handler() {
+    echo
+    echo " ########################################################## "
+    echo
+    echo " An error occurred in:"
+    echo
+    echo " - line number: ${1}"
+    shift
+    echo " - exit status: ${1}"
+    shift
+    echo " - command: ${@}"
+    echo
+    echo " The script will abort now. User input was: "
+    echo
+    echo " ${SCRIPT_NAME} ${SCRIPT_PARAMS}"
+    echo
+    echo " Please copy and paste this error and report it via Git Hub: "
+    echo " https://github.com/cgat-developers/cgat-core/issues "
+    echo " ########################################################## "
+}
+
+trap 'error_handler ${LINENO} $? ${BASH_COMMAND}' ERR INT TERM
+
+# log installation information
+log() {
+    echo "# log | `hostname` | `date` | $1 "
+}
+
+# report error and exit
+report_error() {
+    echo
+    echo $1
+    echo
+    echo "Aborting."
+    echo
+    exit 1
+}
+
+for f in `ls tests/*.py` ;
+do
+    env_f=`sed 's/.py/.yml/g'`
+    log " Testing: python3 conda-deps.py $f"
+    diff <(python3 conda-deps.py $f) <(cat $env_f)
+    if [[ "$?" -eq "0" ]] ; then
+        log " Test succeeded for: $f!"
+    else
+        report_error " Test failed for: $f"
+    fi
+done
