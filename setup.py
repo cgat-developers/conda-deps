3dfe8ff87371f61d4af7566f85d87d112c9dd3a3
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2019-03-18 19:52:50 +0100

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# Reference:
+# https://packaging.python.org/tutorials/packaging-projects/
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conda-deps",
+    version="0.0.1",
+    author="cgat-developers",
+    author_email="sebastian.luna.valero@gmail.com",
+    description="Generate conda environment files from Python source code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
